a1de758fc877ac3f69fdd5f74b2e7885430c5ed7
rodsiqbr <rodsiq_br@hotmail.com>
2025-07-17 15:43:04 -0300

    MSXDev24 final version

--- AlienMSX/targetconfig.s
+++ AlienMSX/targetconfig.s
@@ -1,6 +1,6 @@
 ;-------------------------------------------------		
 ; targetconfig.s created automatically by make.bat		
-; on 15:03:42,23 , 17/07/2025 				
+; on 15:37:34,38 , 17/07/2025 				
 ;														
 ; DO NOT BOTHER EDITING THIS.							
 ; ALL CHANGES WILL BE LOST.							
